9b06b04bcebd29dcdb573cc75bcdbda209517fa2
Fayyad Shhadeh <fayadsh33.sh@gmail.com>
2024-06-13 18:11:58 +0300

    category slice extrareducer

--- src/store/categories/categoriesSlice.ts
+++ src/store/categories/categoriesSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import actGetCategories from "./act/actGetCategories";
 
 interface categories {
     records:{
@@ -19,7 +20,24 @@ const initialState : categories = {
 const categorySlice = createSlice({
     name:"categories",
     initialState,
-    reducers:{}
+    reducers:{},
+    extraReducers(builder) {
+        builder.addCase(actGetCategories.pending ,(state) => {
+            state.loading = 'pending'
+            state.error = null
+        })
+        builder.addCase(actGetCategories.fulfilled ,(state,action) => {
+            state.loading = 'succeeded'
+            state.records = action.payload
+        })
+        builder.addCase(actGetCategories.rejected ,(state,action) => {
+            state.loading = 'failed'
+            if(action.payload && typeof action.payload === 'string'){
+                state.error = action.payload
+            }
+        })
+    },
 })
 
+export {actGetCategories}
 export default categorySlice.reducer
