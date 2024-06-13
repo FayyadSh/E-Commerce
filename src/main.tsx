62ef91c0d9f4390fb31632e801389781fb97f9d2
Fayyad Shhadeh <fayadsh33.sh@gmail.com>
2024-06-13 17:35:19 +0300

    Create App Store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import ReactDOM from 'react-dom/client'
+
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@styles/global.css'
+
 // Imports
 import AppRouter from '@routes/AppRouter'
+import { Provider} from 'react-redux'
+import { store } from './store'
 
 
 const root = document.getElementById('root')!
-ReactDOM.createRoot(root).render(<AppRouter />)
+ReactDOM.createRoot(root).render(
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+  
+)
